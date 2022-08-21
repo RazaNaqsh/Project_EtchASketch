0b289f497ecc8660a2eb88353b3fb0fe76d63d1e
Raza <owaisraza1704@gmail.com>
2022-08-21 19:47:06 +0530

    A nice pseudocode to follow

---
+++ script.js
@@ -0,0 +1,8 @@
+
+//pesudocode
+// make divs, size them up in container, add color when hovered, change size, set color to random
+
+//first thing we need is to create a function to create divs but calling them now would make them appear in column, so we will call it with another function to size it aswell, so we can make a function which will call both of these function and let this be function be called DefaultGrid.
+// another thing in the function which makes div, the scope will be inside this function only, so we needa add the hover class in here only, when these created divs are hovered.
+// thus previous step will call addcolor function aswell, which will add color style property into that element.
+// last thing would be a prompt to change size, here we needa make function taking input as prompt and set a condition that max size would be 100 only, if its not then return prompt again, and if the condition is fulfilled then delete the previous grid and make new grid, by calling make div func and size up func here.
